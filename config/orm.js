4d5e563421d3f8bffb589043ff691beaf845fb05
Wuata <lmgom875@gmail.com>
2020-02-25 19:11:28 -0500

    orm configurations

---
+++ config/orm.js
@@ -0,0 +1,76 @@
+const connection = require("./connection");
+
+
+function printQuestionMarks(num) {
+    let array = [];
+    for (i = 0; i < num; i++) {
+        array.push("?");
+    }
+    return array.toString();
+}
+
+
+function objToSql(ob) {
+    let array = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            array.push(key + "=" + value);
+        }
+    }
+    return array.toString();
+}
+
+let orm = {
+    selectAll: function (tableImput, cb) {
+        let query = "SELECT * FROM " + tableImput + ";";
+        connection.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+            console.log(res);
+        });
+    },
+    insertOne: function (table, colm, valor, cd) {
+        let query = "INSERT INTO " + table;
+        query += " (";
+        query += colm.toString();
+        query += ") ";
+        query += "VALUES (";
+        query += printQuestionMarks(valor.length);
+        query += ")";
+        connection.query(query, valor, function (err, res) {
+            if (err) { throw err };
+            cd(res);
+            console.log(res);
+        })
+    },
+    updateOne: function (table, colm, cond, cd) {
+        let query = "UPDATE " + table;
+        query += " SET ";
+        query += objToSql(colm);
+        query += " WHERE ";
+        query += cond;
+        console.log(query);
+        connection.query(query, function (err, res) {
+            if (err) { throw err };
+            cd(res);
+        });
+    }
+
+}
+
+//let table = "burgers";
+// let columna = "burger_name, devoured";
+// let valortest = { burger_name: "Double Chesse Burger", devoured: false };
+// let valToSql = objToSql(valortest);
+// console.log(valToSql.length);
+// orm.insertOne(table, columna, valToSqls);
+//let objPrueba = { nombre: "Luis Gomez", edad: 43, sexo: "Masculino" };
+
+module.exports = orm;
+
