99136da951f7d8cfaac83df2b4f84f95717e8666
Wuata <lmgom875@gmail.com>
2020-02-26 21:20:21 -0500

    configuration burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,36 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger");
 
-router.get("/", function () {
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        let hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-})
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.param.id;
+    console.log("condition", condition);
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
